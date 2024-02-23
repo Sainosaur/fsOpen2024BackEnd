837df0fdf626023c157c662bb2090e09f7c5999a
adirajus <saiadi4002@gmail.com>
2024-02-23 21:09:51 +0000

    Exercise 4.5 created favoriteBlog function and implemented related testing

--- BlogList/utils/list_helper.js
+++ BlogList/utils/list_helper.js
@@ -2,6 +2,34 @@ const dummy = (blogs) => {
     return 1
 }
 
+const totalLikes = (blogs) => {
+    let init = 0;
+    blogs.map((blog) => init += blog.likes)
+    return init
+}
+
+const favoriteBlog = (blogs) => {
+    let favorite = {
+        likes : 0
+    }
+    blogs.forEach(blog => {
+        if (blog.likes > favorite.likes) {
+            favorite = {
+                title : blog.title,
+                author: blog.author,
+                likes : blog.likes
+            }
+        }
+    })
+    if (favorite.likes != 0) {
+        return favorite
+    } else {
+        return {}
+    }
+}
+
 module.exports = {
-    dummy
+    dummy,
+    totalLikes,
+    favoriteBlog
 }
